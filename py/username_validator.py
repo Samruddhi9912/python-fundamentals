54b56ff802aebf17dd0ab797a9ad37e4e3f21912
Samruddhi KS <samruddhiyadiyal@gmail.com>
2026-08-31 22:58:55 +0530

    Username Validator

---
+++ py/username_validator.py
@@ -0,0 +1,25 @@
+username = input("Enter a username: ")
+
+if (len(username) < 5) :
+    print("Username must have atleast 5 characters")
+elif (len(username) > 15) :
+    print("Username must not exceed 15 characters")
+else:
+    if not(username[0].isalpha()):
+        print("Username must start with a letter")
+    else:
+        num = False
+        for char in username:
+            if char.isdigit():
+                num = True
+                break
+        if (num == False):
+            print("Username must contain atleast one number")
+        else:
+            if " " in username:
+                print("Username must not contain spaces")
+            else:
+                if(username.isalnum() == False):
+                    print("Username must not contain any special characters")
+                else:
+                    print(f"{username} is accepted")
